d3a4b279187ad23b9ef28aae4bcc48ed47092a14
Yuliya Borovaya <Liana-rus81@mail.ru>
2022-08-25 16:53:58 +0300

    Example006_If Else

---
+++ Examples006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "юлия")
+{
+    Console.WriteLine("Ура, это же ЮЛИЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
